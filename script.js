5a7210fafd4bf1684c6928e75005d797f06dcb81
fatimajaved444 <fatimajavedd444@gmail.com>
2024-07-28 01:24:54 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let clock=document.querySelector('#clock');
+
+setInterval(function() {
+    let date=new Date();
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML=(date.toLocaleTimeString())
+}, 1000);
+
